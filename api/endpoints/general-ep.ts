8a85a7dfc47d5a0824379a94cc770f6a48fb0904
VeselinIdov <veselin.idov@columbus.tech>
2025-07-17 12:36:19 +0300

    Add new tests and endpoints for user and admin-related requests

--- api/endpoints/general-ep.ts
+++ api/endpoints/general-ep.ts
@@ -5,6 +5,26 @@ export class GeneralEP extends BaseRequest {
 
     private coreJobsURL: string = "core/jobs";
     private dataSourcesURL: string = "core/data-sources";
+    private adminURL: string = "admin";
+    private userProfileURL: string = "user/profile";
+    private userPreferencesURL: string = "user/preferences";
+    private userPasswordURL: string = "user/password";
+
+    async getUserPasswordRequest(auth: string) {
+        return await this.getRequest(this.userPasswordURL, auth);
+    }
+
+    async getUserPreferencesRequest(auth: string) {
+        return await this.getRequest(this.userPreferencesURL, auth);
+    }
+
+    async getUserProfileRequest(auth: string) {
+        return await this.getRequest(this.userProfileURL, auth);
+    }
+
+    async getAdminRequest(auth: string) {
+        return await this.getRequest(this.adminURL, auth);
+    }
 
     async getDataSourcesRequest(auth: string) {
         return await this.getRequest(this.dataSourcesURL, auth);
